8f37348d4c7595f56d187b0ea4e5bfa7c9b1c226
Victoria Green <vikkicgreen@gmail.com>
2020-07-04 18:19:21 -0400

    day 9 ketchup

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         UserInputUtils.getInputDay();
     }
 }
---
+++ src/main/day9/Recursion3.java
@@ -0,0 +1,38 @@
+package day9;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Recursion3 {
+
+/*
+Write a factorial function that takes a positive integer, N
+as a parameter and prints the result of N!(N factorial).
+*/
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public void day9Challenge() throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        System.out.println(result);
+        scanner.close();
+    }
+
+    //N! .. 4! .. 4 * 3 * 2 * 1
+    static int factorial(int n) {
+        //recursive case
+        if (n > 1) {
+            return n * factorial(n-1);
+        }
+        //base case
+        else {
+            return 1;
+        }
+    }
+
+}
